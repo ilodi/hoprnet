a75f9a80a5f6f4d7736e654318c68152b8f25f90
Beto Salas <alberto.salas@obsidiansoft.io>
2020-10-28 03:00:53 -0600

    Remove unnecessary spaces

--- websites/leaderboard-new/service/pages/index.js
+++ websites/leaderboard-new/service/pages/index.js
@@ -7,8 +7,6 @@ import TrCustom from "../components/micro-components/tr-custom";
 import SuperBoxSearch from "../components/micro-components/super-box-search";
 import api from "../utils/api";
 
-
-
 export default function Home() {
   const columnsDefaults = [
     {
@@ -41,6 +39,7 @@ export default function Home() {
       key: "tweetUrl",
     },
   ];
+  
   const [data, setData] = useState(undefined);
   const [columns, setColumns] = useState(columnsDefaults);
   const [searchTerm, setSearchTerm] = useState("");
@@ -86,7 +85,7 @@ export default function Home() {
         }
       }
     }
-    setMatch(count); 
+    setMatch(count);
   }, [searchTerm]);
 
   const getIntBase = (key) => {
